91a589a74edd337fb90e63d563b7444e40e6e01a
nadsb26 <111922048+nadsb26@users.noreply.github.com>
2026-02-23 01:27:56 +0400

    Add script,js

---
+++ ass-5b/script.js
@@ -0,0 +1,5 @@
+let insideOfBridge = document.getElementById("inside");
+
+insideOfBridge.addEventListener("click", () => {
+    document.getElementById("textbox").innerText = "The Arc de Triomphe is one of the most famous landmarks in Paris, France. Commissioned in 1806 by Napoleon Bonaparte after his victory at Austerlitz, the monument honors the soldiers who fought and died for France during the French Revolutionary and Napoleonic Wars."
+})
